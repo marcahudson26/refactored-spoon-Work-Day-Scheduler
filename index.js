42ee103acf91259069814bd11a05bfbd0402b453
marc hudson <marchudson2601@gmail.com>
2023-01-19 21:09:56 +0000

    added colors to time slots in js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const gradDate = moment();
-$("#currentDay").text(gradDate.format("dddd, D MMM YYYY"));
+//variables for time to change color from hour to hour 
+let dayElem = $("#currentDay");
+let now = moment();
+let today = now.format("LL");
+
+dayElem.text(now.format("dddd, D MMM YYYY"));
+
+let currentHour = Number(now.format("H"));
+
+// this is to set css style for past present & future time blocks  
+$(".row").each(function () {
+    const row = $(this);
+    const id = row.data("id");
+
+
+
+    let hour = Number(id);
+    const timeSlot = $(row.find(".time-slot")[0]);
+
+    if (hour === currentHour) {
+        timeSlot.addClass("present");
+    } else if (hour < currentHour) {
+        timeSlot.addClass("past");
+    } else if (hour > currentHour) {
+        timeSlot.addClass("future");
+    }
+});
 
