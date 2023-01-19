14891644cebb208872bdc0e303786121ba9cab5f
marc hudson <marchudson2601@gmail.com>
2023-01-19 21:46:21 +0000

    added dynamic rendering time blocks with jquery

--- index.js
+++ index.js
@@ -7,7 +7,40 @@ dayElem.text(now.format("dddd, D MMM YYYY"));
 
 let currentHour = Number(now.format("H"));
 
-// this is to set css style for past present & future time blocks  
+/*
+    Example html output:
+    <div class="row" data-id="21">
+      <div class="col-2 time-block hour">9pm</div>
+      <textarea placeholder="Add events here" class="col-8 time-slot calendar-item" rows="2" cols="50"></textarea>
+      <button class="col-2 saveBtn">Confirm</button>
+    </div>
+*/
+
+// add row item to page
+function addRow(id, text) {
+    const row = $("<div>").addClass("row").data("id", id).append(
+        // time-block
+        $("<div>").addClass("col-2 time-block hour").text(text),
+        // time slot
+        $("<textarea>")
+            .addClass("col-8 time-slot calendar-item")
+            .attr("placeholder", "Add events here")
+            .attr("row", "2")
+            .attr("cols", "50"),
+        // confirm
+        $("<button>").addClass("col-2 saveBtn").text("Confirm")
+    );
+    // add new row to container
+    $(".container").append(row);
+}
+
+// loop 9am - 5pm adding row for each time
+for (let i = 9; i <= 17; i++) {
+    const date = moment().set({ hour: i, minute: 0 }).format('h A');
+    addRow(i, date);
+}
+
+//this is to set css style for past present & future time blocks
 $(".row").each(function () {
     const row = $(this);
     const id = row.data("id");
@@ -27,7 +60,7 @@ $(".row").each(function () {
     }
 });
 
-$(".saveBtn button.btn").on("click", function (event) {
+$("button.saveBtn").on("click", function (event) {
     const button = $(this);
     const row = button.closest(".row");
     const id = row.data("id");
